26ec811087ad45fbd3836ef9e6c34e61deb49c6c
Viraj Kanwade <viraj.kanwade@forgeahead.io>
2026-01-11 08:35:52 -0800

    replace black with ruff

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,5 +2,5 @@
 set -x
 
 uv run ruff check src/fastapi_swagger2 tests scripts --fix
-uv run black src/fastapi_swagger2 tests scripts
+uv run ruff format src/fastapi_swagger2 tests scripts
 uv run isort src/fastapi_swagger2 tests scripts
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,5 +5,5 @@ set -x
 
 uv run mypy src/fastapi_swagger2
 uv run ruff check src/fastapi_swagger2 tests scripts
-uv run black src/fastapi_swagger2 tests --check
+uv run ruff format src/fastapi_swagger2 tests scripts --check
 uv run isort src/fastapi_swagger2 tests scripts --check-only
